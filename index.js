3f2b1afc9ac031064c8721c3ce4217b2f3132ff2
manjot <mssond459@gmail.com>
2024-06-15 14:03:32 +0530

    index is added

--- index.js
+++ index.js
@@ -1,19 +1,12 @@
 const express = require('express');
 require('dotenv').config();
-
-
-// Initialize Express app
 const app = express();
-
-// Define a port
 const PORT = process.env.PORT || 3000;
 
-// Define a sample endpoint
 app.get('/', (req, res) => {
     res.send('Hello from Node.js backend!');
 });
 
-// Start the server
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
